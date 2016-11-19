8a877c57c2abadcd3177ef8a1ffb3ebcde9c1bf6
smallfly <hugues@smallfly.com>
2016-11-18 22:12:42 -0500

    Added a demo 360 spherical/equirectangular photo taken with a Nikon Keymission 360.

--- example-360Player/src/ofApp.cpp
+++ example-360Player/src/ofApp.cpp
@@ -11,7 +11,7 @@ void ofApp::setup(){
 	// We need to pass the method we want ofxOpenVR to call when rending the scene
 	openVR.setup(std::bind(&ofApp::render, this, std::placeholders::_1));
 
-	image.load("DSCN0092.JPG");
+	image.load("DSCN0143.JPG");
 	shader.load("sphericalProjection");
 
 	sphere.set(10, 10);
